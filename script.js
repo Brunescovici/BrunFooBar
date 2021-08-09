32175fa333a6691f227e5dae13a3c31124a1687a
Brunescovici <andrei.pricope0@gmail.com>
2021-08-09 03:02:40 +0300

    added the rest of the status of bartenders

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 fetch("https://brunfoobar.herokuapp.com/")
-    .then((response) => response.json())
-    .then((data) => workData(data));
+        .then((response) => response.json())
+        .then((data) => workData(data));
 
-    let i = 0; // number to count the bartenders and give them an id
+window.addEventListener('load', function () {
+    let fetchInterval = 1000;
 
-function workData(x) {
+    setInterval(fetchFct, fetchInterval);
+  });
+
+function fetchFct() {
+    fetch("https://brunfoobar.herokuapp.com/")
+        .then((response) => response.json())
+        .then((data) => updateData(data));
+}
+
+let i = 0; // number to count the bartenders and give them an id
+
+function updateData(x) {    // x is the data got from the database
+    i=0;
+    x.bartenders.forEach(el => {
+        document.querySelector("#worker" + i + " .objText").textContent = el.status;
+        document.querySelector("#worker" + i + " .objStatus").textContent = el.statusDetail;
+        if(el.usingTap != null) {
+            document.querySelector("#worker" + i + " .objTapUsed").textContent = "Using Tap: " + el.usingTap;
+        }
+        if(el.servingCustomer != null)
+            document.querySelector("#worker" + i + " .objCustomer").textContent = "Serving Customer: " + el.servingCustomer;
+        else {
+            document.querySelector("#worker" + i + " .objCustomer").textContent = "";
+        }
+        i++;
+    })
+}
+
+function workData(x) {  // x is the data got from the database
     console.log(x);
     document.getElementById("barName").textContent = x.bar.name;
     x.bartenders.forEach(el => {
@@ -27,21 +56,38 @@ let mouse = false, n = false;
 
 function mouseStatus(idHover) {
     console.log("its working");
-    if(!n)
-        n = true;
-    else
-        n = false;
-    expandWaiter(idHover);
+    if(document.querySelector("#" + idHover + " #downArrow").classList.contains("clickedEl"))
+        collapseWaiter(idHover);
+    else {
+        collapseWaiters();
+        expandWaiter(idHover);
+    }
 }
 
 function expandWaiter(hoverId) {
     el = document.getElementById(hoverId);
-    if(n) {
-        el.style.height = "500px";
-        document.querySelector("#" + hoverId + " #downArrow").classList.add("clickedEl");
-    }
-    else {
+    el.style.height = "auto";
+    document.querySelector("#" + hoverId + " #downArrow").classList.add("clickedEl");
+    document.querySelector("#" + hoverId + " .objStatus").style.display = "block";
+    document.querySelector("#" + hoverId + " .objTapUsed").style.display = "block";
+}
+
+function collapseWaiter(idHov) {
+    document.getElementById(idHov).style.height = "100px"
+    document.querySelector("#" + idHov + " #downArrow").classList.remove("clickedEl");
+    document.querySelector("#" + idHov + " .objStatus").style.display = "none";
+    document.querySelector("#" + idHov + " .objTapUsed").style.display = "none";
+}
+
+function collapseWaiters() {
+    for(j=0; j<document.getElementById("objContainer").childElementCount; j++) {
+        el = document.getElementById("worker" + j);
         el.style.height = "100px"
-        document.querySelector("#" + hoverId + " #downArrow").classList.remove("clickedEl");
+        document.querySelector("#worker" + j + " #downArrow").classList.remove("clickedEl");
+        document.querySelector("#worker" + j + " .objStatus").style.display = "none";
+        document.querySelector("#worker" + j + " .objTapUsed").style.display = "none";
     }
 }
+
+
+
